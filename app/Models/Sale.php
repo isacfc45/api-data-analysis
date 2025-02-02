8bc437e993dc1bb6e50161f61ac7024ad782aa78
isacfc45 <isacferreira2007@hotmail.com>
2025-02-02 18:34:50 -0300

    adiciona autenticação via Sanctum, ajusta redirecionamento de usuários não autenticados e define atributos preenchíveis no modelo Sale

--- app/Models/Sale.php
+++ app/Models/Sale.php
@@ -8,4 +8,16 @@ use Illuminate\Database\Eloquent\Model;
 class Sale extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'user_id',
+        'amount',
+        'product_name',
+        'date',
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
